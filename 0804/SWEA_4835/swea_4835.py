ed5c51d1040cfcb43f051025f1e87b18fc741e8e
junhyeon <jwnsgus@gmail.com>
2020-08-06 13:44:19 +0900

    finish 0806_algorithm

--- 0804/SWEA_4835/swea_4835.py
+++ 0804/SWEA_4835/swea_4835.py
@@ -4,23 +4,40 @@ sys.stdin = open("sample_input.txt", "r")
 
 T = int(input())
 
-for test_case in range(1, T + 1):
-    input_data = list(map(int, input().split()))
-    N = input_data[0] # 10
-    M = input_data[1] # 3
+# for test_case in range(1, T + 1):
+#     N, M = map(int, input().split())
 
-    num_data = list(map(int, input().split()))
+#     num_data = list(map(int, input().split()))
 
-    _min = 9999999999
-    _max = -9999999999
-    for i in range(N+1-M):
-        _sum = 0
-        for k in range(i, i+M):
-            _sum += num_data[k]
+#     _min = 9999999999
+#     _max = -9999999999
+#     for i in range(N+1-M):
+#         _sum = 0
+#         for k in range(i, i+M):
+#             _sum += num_data[k]
 
-        if _min > _sum:
-            _min = _sum
-        if _max < _sum:
-            _max = _sum
+#         if _min > _sum:
+#             _min = _sum
+#         if _max < _sum:
+#             _max = _sum
 
-    print('#{} {}'.format(test_case, _max-_min))
+#     print('#{} {}'.format(test_case, _max-_min))
+
+for t in range(T):
+    N, M = map(int, input().split())
+    num = list(map(int, input().split()))
+
+    cur = 0
+    for i in range(M):
+        cur += num[i]
+
+    max_val = cur
+    min_val = cur
+    for i in range(M, N):
+        cur += num[i] - num[i-M]
+        if max_val < cur:
+            max_val = cur
+        elif min_val > cur:
+            min_val = cur
+    
+    print('#{} {}'.format(t, max_val-min_val))
